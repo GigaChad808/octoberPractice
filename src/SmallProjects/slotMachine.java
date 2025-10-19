78dfdbc46af5330af30419636cdd5d16469ea5c1
GigaChad808 <snamazov14013@ada.edu.az>
2025-10-19 20:30:53 +0400

    writing a small slot machine game

---
+++ src/SmallProjects/slotMachine.java
@@ -0,0 +1,26 @@
+package SmallProjects;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class slotMachine {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+
+        // declare variables
+        // displaying welcome message
+        // play if balance is greater than 0
+        // Enter bet amount
+        //  verify if bet > balance
+        //  verify if bet > 0
+        //  subtract bet from balance
+        // Spin row
+        // print row
+        // get payout
+        // wanna play again?
+        // displaying exit message
+
+    }
+
+}
